894721ee20e25c95843add6b9605c4eaf498dd69
kyle <the.kyle.maune@gmail.com>
2017-10-02 10:50:45 -0700

    working on front and back end communicating

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module IdeaboardApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://casco-ideaboard.herokuapp.com/'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
